98081e464099636dc0ee17f1ba6b9b2f201589ee
JoseEdgar <jsaldarriaga0028@gmail.com>
2021-01-09 21:46:10 -0500

    feat(1a): Crear una estructura escalable y modular del proyecto

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,24 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LayoutComponent } from './layout/layout.component';
+import { HeaderComponent } from './layout/header/header.component';
+import { FooterComponent } from './layout/footer/footer.component';
+import { AdministradorComponent } from './administrador/pages/administrador/administrador.component';
+import { ContactenosComponent } from './contactenos/pages/contactenos/contactenos.component';
+import { CursoComponent } from './curso/pages/curso/curso.component';
+import { HomeComponent } from './home/pages/home/home.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LayoutComponent,
+    HeaderComponent,
+    FooterComponent,
+    AdministradorComponent,
+    ContactenosComponent,
+    CursoComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
